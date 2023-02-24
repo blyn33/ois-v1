cfbe6a1c45d9e09d28671410877602b103dbd916
ZankoB <zb26346@student.uni-lj.si>
2023-02-24 11:17:47 +0100

    Dodaj izracun pospeska

--- Gravitacija.java
+++ Gravitacija.java
@@ -9,7 +9,6 @@ public class Gravitacija {
 		double C = 6.674 * Math.pow(10, -11);
 		double M = 5.972 * Math.pow(10, 24);
 		double R = 6.371 * Math.pow(10, 6);
-		double V = sc.nextDouble();
 
 		double A = (C * M) / ((R + V) * (R + V));
 		return A;
