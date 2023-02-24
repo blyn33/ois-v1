6860c6c5e19353b90d2ae0cb758172c8ef0a21ff
ZankoB <zb26346@student.uni-lj.si>
2023-02-24 11:14:54 +0100

    Dodaj izracun pospeska

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,16 +1,17 @@
-import java.util.Scanner;
 
 public class Gravitacija {
 
-    public static void main(String[] args) {
-        System.out.println("OIS je zakon!");
-		Scanner sc = new Scanner(System.in);
-		
+	public static void main(String[] args) {
+		System.out.println("OIS je zakon!");
+
+	}
+	public int pospesk(int V) {
 		double C = 6.674 * Math.pow(10, -11);
 		double M = 5.972 * Math.pow(10, 24);
 		double R = 6.371 * Math.pow(10, 6);
 		double V = sc.nextDouble();
-		
+
 		double A = (C * M) / ((R + V) * (R + V));
-    }
+		return A;
+	}
 }
