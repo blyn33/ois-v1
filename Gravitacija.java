a63ff10f1b5a38cc40387a0c48f32e87c884e0c2
lolek <gregor.bucar99@gmail.com>
2023-02-24 10:17:21 +0100

    dodajanje izpisa

--- Gravitacija.java
+++ Gravitacija.java
@@ -3,4 +3,8 @@ public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon!");
     }
+    public static void izpisi(double nadmorskaVisina, double gravitacijskiPospesek) {
+        System.out.println("Nadmorska visina: " + nadmorskaVisina + " m");
+        System.out.println("Gravitacijski pospesek: " + gravitacijskiPospesek + " m/s^2");
+    }
 }
