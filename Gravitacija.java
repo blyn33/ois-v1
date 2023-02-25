9ba6ab48c877526ddc2b2f6ec914d6712e36449d
blyn33 <125354347+blyn33@users.noreply.github.com>
2023-02-25 22:01:00 +0100

    Gravitacija.java

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,19 +1,24 @@
+import java.util.*;
 
 public class Gravitacija {
 
 	public static void main(String[] args) {
 		System.out.println("OIS je zakon!");
+		
+		double nadmorskaVisina = sc.nextDouble();
 
+		izpisi(nadmorskaVisina, pospesk(nadmorskaVisina));
 	}
-	public int pospesk(int V) {
+	public double pospesk(double V) {
 		double C = 6.674 * Math.pow(10, -11);
 		double M = 5.972 * Math.pow(10, 24);
 		double R = 6.371 * Math.pow(10, 6);
 
 		double A = (C * M) / ((R + V) * (R + V));
+		
 		return A;
 	}
-	
+
     public static void izpisi(double nadmorskaVisina, double gravitacijskiPospesek) {
         System.out.println("Nadmorska visina: " + nadmorskaVisina + " m");
         System.out.println("Gravitacijski pospesek: " + gravitacijskiPospesek + " m/s^2");
